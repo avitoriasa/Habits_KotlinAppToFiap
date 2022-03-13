687e5f6953d5a42ee790ff1b059a0878a6d54e27
Vitória Sá <vitoriafs98@outlook.com>
2022-03-12 23:27:47 -0300

    db e API

---
+++ app/src/main/java/br/com/fiap/habits/RetrofitService.kt
@@ -0,0 +1,16 @@
+package br.com.fiap.habits
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RetrofitService {
+
+    @GET("{CEP}/json/")
+    fun getCEP(@Path("CEP") CEP : String) : Call<CEP>
+
+    @GET("{estado}/{cidade}/{endereco}/json/")
+    fun getRCE(@Path("estado") estado: String,
+               @Path("cidade") cidade: String,
+               @Path("endereco") endereco: String): Call<List<CEP>>
+}
